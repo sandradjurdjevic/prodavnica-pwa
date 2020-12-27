103cb58ef5e8f6dbc55a9bce0878dff554fa6a68
sandradjurdjevic <sandradjurdjevic998@gmail.com>
2020-12-27 12:04:41 +0100

    l

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
     
     window.addEventListener('load',()=>{
       navigator.serviceWorker
-      .register("sw_cache_site.js",{ scope: "'/prodavnica-pwa/'" })
+      .register("sw_cache_site.js",{ scope: "/prodavnica-pwa/" })
       .then(reg => console.log('Service Worker:Registered'))
       .catch(err => console.log('Service Worker: Error: ${err}'));
     })
